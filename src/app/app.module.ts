c41c5dff6475f568cb40d65fef4e95a512f73b37
Janne Aikioniemi <janne.aikioniemi@siili.com>
2019-05-14 12:28:51 +0300

    FIX: Merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import { AlertifyService } from './_services/alertify.service';
-=======
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
->>>>>>> 8a1890bb1c79a041f52652976bdcd13f37270c28
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -32,11 +29,8 @@ import { HomeComponent } from './home/home.component';
    ],
    providers: [
       AuthService,
-<<<<<<< HEAD
-      AlertifyService
-=======
+      AlertifyService,
       ErrorInterceptorProvider
->>>>>>> 8a1890bb1c79a041f52652976bdcd13f37270c28
    ],
    bootstrap: [
       AppComponent
