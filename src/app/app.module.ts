00524dcd5686c5da26af702d2da62d30d41e50cf
Janne Aikioniemi <janne.aikioniemi@siili.com>
2019-05-14 13:52:13 +0300

    Adds NGX dropdown to nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { AlertifyService } from './_services/alertify.service';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
+import { AlertifyService } from './_services/alertify.service';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AuthService } from './_services/auth.service';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +26,8 @@ import { HomeComponent } from './home/home.component';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
