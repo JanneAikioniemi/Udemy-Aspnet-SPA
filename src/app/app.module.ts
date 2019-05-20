cf7dc77c4d5b6d596cc22d10aeb2f2402856aec3
Janne Aikioniemi <janne.aikioniemi@siili.com>
2019-05-20 10:46:58 +0300

    Adds JwtModule for token handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { MemberCardComponent } from './members/member-card/member-card.component';
-import { AuthGuard } from './_guards/auth.guard';
+import { JwtModule } from '@auth0/angular-jwt';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
-
-import { AlertifyService } from './_services/alertify.service';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { AuthService } from './_services/auth.service';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+import { AuthService } from './_services/auth.service';
+import { UserService } from './_services/user.service';
+import { AuthGuard } from './_guards/auth.guard';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -19,7 +19,12 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+
+
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -37,7 +42,14 @@ import { UserService } from './_services/user.service';
       AppRoutingModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter,
+            whitelistedDomains: ['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
