c3035be4795b21218002362ad99d3695018ae574
Janne Aikioniemi <janne.aikioniemi@siili.com>
2019-05-27 10:59:48 +0300

    Expands the register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -22,7 +22,12 @@ export class RegisterComponent implements OnInit {
   createRegisterForm() {
     // Validators are for demoing purposes as password shouldn't have max length
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', Validators.required]
     }, { validator: this.passwordMatchValidator });
